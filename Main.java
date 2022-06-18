63ac27316635d6ac8b6b0dea584813b2cbbd8112
Bartek5756 <101180607+Bartek5756@users.noreply.github.com>
2022-06-18 18:23:26 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+class Main 
+{
+  public static void main(String[] args) 
+    {
+      int a = 8;
+      if(a<10)
+        System.out.println("a<10");
+//      if (a==10)
+//        System.out.println("a=10");
+//      if (a==9)
+//        System.out.println("a=9");
+      if (a==8)
+        System.out.println("a=8");
+  //    else
+    //    System.out.println("a>10");
+
+      int b= 7;
+      
+    switch (b)
+      {
+        case 1:
+            System.out.println("b=1");
+            break;
+        case 2:
+            System.out.println("b=2");
+            break;
+        case 3:
+            System.out.println("b=3");
+            break;
+        case 4:
+            System.out.println("b=4");
+            break;
+        case 5:
+            System.out.println("b=5");
+            break;
+      }
+
+      int  c = 9;
+      int d;
+      d = c < 10 ? -1 : 1;
+      System.out.println(d);
+
+      for (int i = 0; i<10; i++)
+        {
+          System.out.print(i +" ");
+          if (i==5)
+            break;
+        }
+
+      System.out.println();
+
+      for (int i = 0; i<10; i++)
+        {
+          if(i%2==0) continue;
+          System.out.print(i +" ");
+        }
+
+      Scanner scanner = new Scanner (System.in);
+
+      String name = "";
+
+      scanner.next();
+
+      System.out.println(name);
+    }
+}
